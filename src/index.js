779bd7ad51b0a0082be94ebc4d03f574a646c673
Ta-tiana <ustinya88@gmail.com>
2019-10-04 00:49:44 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let newKey;
+
+    for (let key in MORSE_TABLE) {
+        let value = MORSE_TABLE[key];
+
+        newKey = key.replace(/\./g, 10).replace(/-/g, 11);
+
+        if (newKey.length < 10) {
+            let countNull = 10 - newKey.length;//add nulls to keys which less than 10
+            newKey = "0".repeat(countNull) + newKey;
+        }
+        MORSE_TABLE[newKey] = value;
+    }
+
+    let mas = expr.match(/.{1,10}/g); // split string EXPR to parts by 10 symbols, g -global split not once but whole string
+    let result = '';
+
+    a:  for (let m of mas) {
+        for (let o in MORSE_TABLE) {
+            if (o === m) {
+                result += MORSE_TABLE[o];
+            }
+
+            if (m === '**********') { // if in EXPR exists '**********' than push spacebar
+                result += ' ';
+                continue a;
+            }
+        }
+    }
+    return result;
 }
 
 module.exports = {
     decode
-}
+};
